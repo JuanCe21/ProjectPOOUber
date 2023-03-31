491efac4d631bbfac1387acbe0f44f27b42be6d4
Juan Ceron <juuanc2@gmail.com>
2023-03-30 22:36:01 -0500

    new files 2

--- PHP/Index.php
+++ PHP/Index.php
@@ -2,6 +2,12 @@
 
 require_once('Car.php');
 require_once('Account.php');
+require_once("uberX.php");
+require_once("uberPool.php");
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->printDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->PrintDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Charger");
+$uberPool->PrintDataCar();
+?>
